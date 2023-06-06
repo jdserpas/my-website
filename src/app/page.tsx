b3d1d5d8a7991a52bd060d5444629c15046c3fff
Josh Serpas <jdserpas@gmail.com>
2023-06-06 13:59:31 -0400

    refactored image to be in seperate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
 import './globals.css'
-import Image from 'next/image'
-import construction from '../../public/under-construction_geek_man_01.svg'
+import Construction from '@/components/construction'
 
 export default function Home() {
   return (
     <main className='background flex justify-center items-center h-screen'>
-      <div className='w-80'>
-        <Image src={construction} alt="The current page is not yet finished" className='mx-auto'/>
-      </div>
+      <Construction/>
     </main>
   )
 }
