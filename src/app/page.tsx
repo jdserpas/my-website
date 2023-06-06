5b68cd7f8d2612690726e2c378c3b84008ff881f
Josh Serpas <jdserpas@gmail.com>
2023-06-06 13:50:58 -0400

    added construction image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import './globals.css'
+import Image from 'next/image'
+import construction from '../../public/under-construction_geek_man_01.svg'
 
 export default function Home() {
   return (
-    <main className='background'>
-      
+    <main className='background flex justify-center items-center h-screen'>
+      <div className='w-80'>
+        <Image src={construction} alt="The current page is not yet finished" className='mx-auto'/>
+      </div>
     </main>
   )
 }
