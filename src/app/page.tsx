23796447688c47376764eee1634a3594de29aee4
Josh Serpas <jdserpas@gmail.com>
2023-06-09 19:38:11 -0400

    first pass at navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import NavBar from '@/components/navbar'
 import './globals.css'
 import Construction from '@/components/construction'
 
 export default function Home() {
   return (
     <main className='background'>
+      <NavBar/>
       <Construction/>
     </main>
   )
