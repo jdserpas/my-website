d92f49e086d76350ad734e2ad7762d28050e0935
Josh Serpas <jdserpas@gmail.com>
2023-06-12 12:42:52 -0400

    fixed failing tests

--- src/components/__test__/navbar.test.tsx
+++ src/components/__test__/navbar.test.tsx
@@ -1,6 +1,20 @@
 import { render } from "@testing-library/react";
 import NavBar from "../navbar";
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
 describe('NavBar', () => {
   it(`let's the user download my resume`, ()=> {
     //arrange
