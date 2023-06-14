386adac52e5a41e54d73defaa5c6b78596cc5df5
Josh Serpas <43308316+jdserpas@users.noreply.github.com>
2023-06-14 00:09:48 -0400

    Feature/elevator pitch (#7)
    * added first pass at typewriter summary
    
    * typewriter styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import NavBar from '@/components/navbar'
 import './globals.css'
 import Construction from '@/components/construction'
+import ElevatorPitch from '@/components/elevatorPitch'
 
 export default function Home() {
   return (
     <main className='background'>
       <NavBar/>
-      <Construction/>
+      <ElevatorPitch/>
     </main>
   )
 }
---
+++ src/components/elevatorPitch.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { Box, Flex, Text } from '@chakra-ui/react';
+import { delay } from 'framer-motion';
+import Typewriter from 'typewriter-effect';
+
+export default function ElevatorPitch() {
+  return(
+    <Flex h={'80vh'}>
+      <Box fontSize={{base:'3xl' ,sm:'4xl',md:'5xl', lg:'6xl'}} fontFamily={`'Courier New', monospace`} fontWeight={'600'} color={'whiteAlpha.900'} px={'20'} py={'10'}>
+        <Typewriter
+          options={{delay: 75}}
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(`Hi! `)
+              .pauseFor(1500)
+              .typeString(`I'm Josue Se`)
+              .deleteChars(5)
+              .typeString('h Serpas. ')
+              .pauseFor(1000)
+              .typeString('I make computers do cool things for a living...')
+              .pauseFor(500)
+              .typeString(`Wanna know how?\n Download my <u><a class="typewriter-link" href='/document/Josh-Serpas-Resume.pdf' download>Resume<a/></u>.`)
+              .start();
+          }}
+        />
+      </Box>
+    </Flex>
+  )
+}
